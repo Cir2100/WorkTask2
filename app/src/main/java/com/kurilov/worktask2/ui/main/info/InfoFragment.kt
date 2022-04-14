72539dd03f181da6061b7e32314f788d5987fb54
Cir2100 <kurilov.sereja2012@yandex.ru>
2022-04-14 19:41:48 +0300

    Initial commit

---
+++ app/src/main/java/com/kurilov/worktask2/ui/main/info/InfoFragment.kt
@@ -0,0 +1,37 @@
+package com.kurilov.worktask2.ui.main.info
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.kurilov.worktask2.R
+import com.kurilov.worktask2.databinding.FragmentInfoBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class InfoFragment : Fragment() {
+
+    private var _binding: FragmentInfoBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentInfoBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
